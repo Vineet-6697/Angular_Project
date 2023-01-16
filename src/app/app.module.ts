4e1949f6d3455359231791c3f33d9bda5241faa1
Vineet Thakur <bhativineet017@gmail.com>
2023-01-16 18:45:05 +0530

    Binding | class,attribute,property,style and event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,26 +7,24 @@ import {MatInputModule} from '@angular/material/input';
 import { CourseComponent } from './course.component';
 import { LoginModule } from './login/login.module';
 import { NavbarModule } from './navbar/navbar.module';
-import { FooterComponent } from './footer/footer.component';
 import { MainService } from './main.service';
-
-
-
-
+import { NgModule } from '@angular/core';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
+    NavbarModule,
     LoginModule,
-    NavbarModule
+    FooterModule,
+    
   ],
   providers: [MainService],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { MainService } from '../main.service';
 import { LoginService } from './login.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,27 @@ import { LoginService } from './login.service';
 })
 export class LoginComponent implements OnChanges, OnInit,   OnDestroy {
   toggle:boolean=true;
+  @Input() item:any;
   calltoggle(){
     this.toggle= this.toggle?false:true;
   }
+btnsave(){
+  console.log("Form Data", this.form);
+}
+  form:any={
+    "name": "",
+    "email": "",
+    "city": "",
+    "password":""
+      }
+
+     
   constructor(_services:MainService, private loginservice:LoginService, ){
     //  let courses = new MainService();
     console.log("courses", _services.getcourses());
     console.log(this.loginservice.setlogindetail().city);
+
+    
 
     
   }
@@ -32,4 +47,8 @@ export class LoginComponent implements OnChanges, OnInit,   OnDestroy {
   ngOnDestroy(): void {
     console.log('inside destroy');
   }
+  
+  
 }
+
+
